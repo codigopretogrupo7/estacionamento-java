921fd1b9c1215cba5eb5e8a5328ac77b8d82dd96
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-08-20 11:20:20 -0400

    Update UsuarioControllerTest.java

--- src/test/java/br/com/temvaga/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/temvaga/controller/UsuarioControllerTest.java
@@ -1,25 +1,65 @@
 package br.com.temvaga.controller;
 
+
+import br.com.temvaga.model.Usuario;
 import br.com.temvaga.service.UsuarioService;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(SpringExtension.class)
-class UsuarioControllerTest {
 
-    @InjectMocks
-    private UsuarioController usuarioController;//testa as classes dentro do controler
 
-    @Mock
-    private UsuarioService usuarioServiceMock;
+
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WithMockUser(username = "root")
+
+@WebMvcTest(UsuarioController.class)
+public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UsuarioService usuarioService;
+
+
 
 
     @Test
-    void test(){}
+    public void deveRetornarSucesso_QuandoBurcaUsuárioPorNome() throws Exception{
+        Usuario user = new Usuario();
+        Mockito.when(usuarioService.usuarioByNameUser("username")).thenReturn(new ResponseEntity<Usuario>(user, HttpStatus.OK));
+        this.mockMvc.perform(
+                        get("/api/usuarios/list/name")
+                                .queryParam("username","Cristala")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+
+    }
+
+
+
 
 }
+
+
+
+
+
